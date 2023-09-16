452ad63c453864a9ccc687d85cce28e3234afed3
9OSU1G02 <huyquocnguyen1902@gmail.com>
2023-09-16 15:53:33 +0700

    Initial Commit

---
+++ ChatSocketIO/ChatSocketIOApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatSocketIOApp.swift
+//  ChatSocketIO
+//
+//  Created by 9OSU1G02 on 9/16/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatSocketIOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
